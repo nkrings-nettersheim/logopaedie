7861528e02f2bc81e6735d87cad68dc510af684e
Norbert Krings <norbert.krings@t-online.de>
2025-03-13 15:00:05 +0100

    4.7 Warteliste Erweiterung

--- reports/models.py
+++ reports/models.py
@@ -72,7 +72,7 @@ class Patient(models.Model):
     pa_fr = models.CharField(max_length=50, blank=True, default='')
     pa_sa = models.CharField(max_length=50, blank=True, default='')
     pa_appointment = models.CharField(max_length=200, blank=True, default='', null=True)
-    pa_wiedervorstellung_info = models.CharField(max_length=254, default='')
+    pa_wiedervorstellung_info = models.CharField(max_length=254, default='', null=True, blank=True)
 
     def __str__(self):
         return self.pa_last_name + ' ' + self.pa_first_name + '; ' + self.pa_city
