51695f829fd684c20ee53c2baa33d5d682392254
Norbert Krings <norbert.krings@t-online.de>
2026-09-13 11:49:13 +0200

    admin.py erweitert für history

--- parents/admin.py
+++ parents/admin.py
@@ -3,4 +3,9 @@ from simple_history.admin import SimpleHistoryAdmin
 
 from parents.models import Parents_sheet
 
-admin.site.register(Parents_sheet, SimpleHistoryAdmin)
+
+class ParentsSheetAdmin(SimpleHistoryAdmin):
+    list_display = ('id', 'child_last_name', 'child_first_name', 'sheet_created')
+
+
+admin.site.register(Parents_sheet, ParentsSheetAdmin)
--- reports/admin.py
+++ reports/admin.py
@@ -4,25 +4,40 @@ from .models import Patient, Therapy, Therapy_report, Process_report, Doctor, Th
 from .models import Document, Document_therapy, Diagnostic_group, Wait_list, Login_Failed, Shortcuts, Registration
 
 
+class PatientAdmin(SimpleHistoryAdmin):
+    list_display = ('id', 'pa_last_name', 'pa_first_name', 'pa_date_of_birth')
+
 class TherapyAdmin(SimpleHistoryAdmin):
-    list_display = ('recipe_date', 'patients')
+    list_display = ('id', 'recipe_date', 'patients')
+
+class TherapyReportAdmin(SimpleHistoryAdmin):
+    list_display = ('id', 'therapy', 'report_date')
+
+class ProcessReportAdmin(SimpleHistoryAdmin):
+    list_display = ('id', 'therapy', 'process_treatment', 'created_at')
+
+class InitialAssessmentAdmin(SimpleHistoryAdmin):
+    list_display = ('id', 'therapy', 'ia_assessment', 'ia_date')
+
+class RegistrationAdmin(SimpleHistoryAdmin):
+    list_display = ('id', 'reg_name', 'reg_first_name', 'reg_created')
 
 class DoctorAdmin(admin.ModelAdmin):
     list_display = ('doctor_name1', 'doctor_name2', 'doctor_city')
 
 admin.site.site_header = "Admin Bereich Logopädische Praxis Schumacher"
 
-admin.site.register(Patient, SimpleHistoryAdmin)
+admin.site.register(Patient, PatientAdmin)
 admin.site.register(Therapy, TherapyAdmin)
-admin.site.register(Therapy_report, SimpleHistoryAdmin)
-admin.site.register(Process_report, SimpleHistoryAdmin)
+admin.site.register(Therapy_report, TherapyReportAdmin)
+admin.site.register(Process_report, ProcessReportAdmin)
 admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(Therapist)
-admin.site.register(InitialAssessment, SimpleHistoryAdmin)
+admin.site.register(InitialAssessment, InitialAssessmentAdmin)
 admin.site.register(Document)
 admin.site.register(Document_therapy)
 admin.site.register(Diagnostic_group)
 admin.site.register(Wait_list)
 admin.site.register(Login_Failed)
 admin.site.register(Shortcuts)
-admin.site.register(Registration, SimpleHistoryAdmin)
+admin.site.register(Registration, RegistrationAdmin)
